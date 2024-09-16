d48ae2f38693a5f173eaeb71261d8f6cb1e9d1e7
IsMa <itorre13@hotmail.com>
2024-09-16 09:13:21 +0200

    proyecto completo back

--- db.js
+++ db.js
@@ -1,10 +1,11 @@
+import dotenv from 'dotenv';
 import{MongoClient, ObjectId} from 'mongodb';
 
-const url = "mongodb+srv://itorre13:mongoItorre31@prueba.f6rn9.mongodb.net/";
+dotenv.config();
 
 function conectar(){
-    return MongoClient.connect(url);
-};
+    return MongoClient.connect(process.env.MONGO_URL);
+}
 
 export function leerTareas(){
     return new Promise(async (ok,ko) => {
@@ -15,9 +16,10 @@ export function leerTareas(){
 
             let tareas = await coleccion.find({}).toArray();
 
-            ok(tareas.map(({_id,tarea}) =>{
-                let id = _id;
-                return {id,tarea};
+            conexion.close();
+
+            ok(tareas.map(({_id,tarea,terminada}) => {
+                return { id : _id, tarea, terminada }
             }));
 
         }catch(error){
@@ -25,3 +27,123 @@ export function leerTareas(){
         }
     });
 }
+
+/* PRUEBA leerTareas()
+leerTareas()
+.then(x => console.log(x))
+.catch(x => console.log(x));
+*/
+ 
+
+export function crearTarea(texto){
+    return new Promise(async (ok,ko) => {
+        try{
+            const conexion = await conectar();
+
+            const coleccion = conexion.db("tareas").collection("tareas");
+
+            let {insertedId} = await coleccion.insertOne({ tarea : texto, terminada : false });
+
+            conexion.close();
+
+            ok(insertedId);
+
+        }catch(error){
+            ko({ error : "error en la base de datos"});
+        }
+    });
+}
+
+/* PRUEBA crearTarea()
+crearTarea("Otra tarea nueva...probando crearTarea")
+.then(x => console.log(x))
+.catch(x => console.log(x));
+*/
+
+export function borrarTarea(id){
+    return new Promise(async (ok,ko) => {
+        try{
+            const conexion = await conectar();
+
+            const coleccion = conexion.db("tareas").collection("tareas");
+
+            let {deletedCount} = await coleccion.deleteOne({ _id : new ObjectId(id) });
+
+            conexion.close();
+
+            ok(deletedCount);
+
+        }catch(error){
+            ko({ error : "error en la base de datos"});
+        }
+    });
+}
+
+/* PRUEBA borrarTarea()
+borrarTarea('66e699ab99df372c10f519e2')
+.then(x => console.log(x))
+.catch(x => console.log(x));
+ */
+
+export function actualizarTexto(id,texto){
+    return new Promise(async (ok,ko) => {
+        try{
+            const conexion = await conectar();
+
+            const coleccion = conexion.db("tareas").collection("tareas");
+
+            let {modifiedCount} = await coleccion.updateOne({ _id : new ObjectId(id) }, { $set : { tarea : texto }});
+
+            conexion.close();
+
+            ok(modifiedCount);
+
+        }catch(error){
+            ko({ error : "error en la base de datos"});
+        }
+    });
+}
+
+/* PRUEBA actualizarTexto()
+actualizarTexto('66e6998912256b927c657b6c', "blo blo blo")
+.then(x => console.log(x))
+.catch(x => console.log(x));
+ */
+
+export function actualizarEstado(id){
+    return new Promise(async (ok,ko) => {
+        try{
+            const conexion = await conectar();
+
+            const coleccion = conexion.db("tareas").collection("tareas");
+
+            let {terminada} = await coleccion.findOne({ _id : new ObjectId(id) });
+
+            let {modifiedCount} = await coleccion.updateOne({ _id : new ObjectId(id) }, { $set : { terminada : !terminada }});
+
+            conexion.close();
+
+            ok(modifiedCount);
+
+        }catch(error){
+            ko({ error : "error en la base de datos"});
+        }
+    });
+}
+
+/* PRUEBA actualizarEstado()
+actualizarEstado('66e6998912256b927c657b6c')
+.then(x => console.log(x))
+.catch(x => console.log(x));
+ */
+
+
+/* PRUEBA leerTareas()
+leerTareas()
+.then(x => console.log(x))
+.catch(x => console.log(x));
+*/
+
+
+
+
--- index.js
+++ index.js
@@ -1,29 +1,107 @@
 import express from 'express';
-import { config } from 'dotenv';
-// import cors from 'cors';
-// import { leerTareas } from './db.js';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import { leerTareas,crearTarea,borrarTarea,actualizarTexto,actualizarEstado } from './db.js';
+
+dotenv.config();
 
 const servidor = express();
 
-config();
-
-/*
 servidor.use(cors());
-servidor.use(express.json());
 
-servidor.get("/", async (peticion,respuesta) => {
-    try{
-        let tareas = await leerTareas();
-        respuesta.json(tareas)
-    }catch(error){
-        respuesta.status(500);
-        respuesta.json(error);
-    }
-});
-*/
+servidor.use(express.json());
 
 if(process.env.TEST){  // si process.env.TEST existe
     servidor.use("/pruebas",express.static("./prueba")) // le decimos al servidor que queremos crear a partir de /pruebas express.static la carpeta. Si ponemos en localhost:3000/pruebas voy a acceder a lo que contenga el directorio estático ./prueba
 }
 
+servidor.get("/tareas", async (peticion,respuesta) => {
+    try{
+        let tareas = await leerTareas();
+        respuesta.json(tareas);
+    }catch(error){
+        respuesta.status(500);
+        respuesta.json({ error : "error en la base de datos"});
+    }
+});
+
+servidor.post("/tareas/nueva", async (peticion,respuesta,siguiente) => {
+    try{
+        
+        if(peticion.body.tarea && peticion.body.tarea.trim() != ""){
+            let {tarea} = peticion.body;
+
+            let id = await crearTarea(tarea);
+
+            return respuesta.json({id});
+
+        }
+
+        siguiente("error");
+
+
+    }catch(error){
+        respuesta.status(500);
+        respuesta.json({ error : "error en la base de datos"});
+    }
+});
+
+servidor.delete("/tareas/borrar/:id([0-9a-f]{24})", async (peticion,respuesta) => {
+    try{
+        let cantidad = await borrarTarea(peticion.params.id);
+
+        respuesta.json({ resultado : cantidad ? "ok" : "ko" });
+
+    }catch(error){
+        respuesta.status(500);
+        respuesta.json({ error : "error en la base de datos" });
+    }
+});
+
+servidor.put("/tareas/actualizar/texto/:id([0-9a-f]{24})", async (peticion,respuesta,siguiente) => {
+    try{
+
+        if(peticion.body.tarea && peticion.body.tarea.trim() != ""){
+
+            let {tarea} = peticion.body;
+        
+            let cantidad = await actualizarTexto(peticion.params.id,tarea);
+
+        return respuesta.json({ resultado : cantidad ? "ok" : "ko" });
+
+        }
+
+        siguiente("error");
+
+    }catch(error){
+        respuesta.status(500);
+        respuesta.json({ error : "error en la base de datos" });
+    }
+});
+
+servidor.put("/tareas/actualizar/estado/:id([0-9a-f]{24})", async (peticion,respuesta) => {
+    try{    
+        let cantidad = await actualizarEstado(peticion.params.id);
+
+        respuesta.json({ resultado : cantidad ? "ok" : "ko" });
+
+    }catch(error){
+        respuesta.status(500);
+        respuesta.json({ error : "error en la base de datos" });
+    }
+});
+
+servidor.use((error,peticion,respuesta,siguiente) => {
+    respuesta.status(400);
+    respuesta.json({ error : "error en la petición" });
+});
+
+servidor.use((peticion,respuesta) => {
+    respuesta.status(404);
+    respuesta.json({ error : "recurso no encontrado" });
+});
+
 servidor.listen(process.env.PORT); // en el puerto que he configurado en el fichero .env
+
+
+
